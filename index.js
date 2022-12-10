635db954e5e93327bcfd9ca2eab15de42c667636
Siam10101 <mehedihasansiam030@gmail.com>
2022-12-10 10:21:17 +0600

    facebook clone make

---
+++ index.js
@@ -0,0 +1,32 @@
+//toggle btn
+var settingsmenu = document.querySelector(".settings-menu");
+
+function settingsMenuToggle() {
+  settingsmenu.classList.toggle("settings-menu-height");
+}
+
+//dark btn
+
+var darkBtn = document.getElementById("dark-btn");
+darkBtn.onclick = function () {
+  darkBtn.classList.toggle("dark-btn-on");
+
+  document.body.classList.toggle("dark-theme");
+
+  if (localStorage.getItem("theme") == "light") {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+};
+
+//permanently dark mode start here
+if (localStorage.getItem("theme") == "light") {
+  darkBtn.classList.remove("dark-btn-on");
+  document.body.classList.remove("dark-theme");
+} else if (localStorage.getItem("theme") == "dark") {
+  darkBtn.classList.add("dark-btn-on");
+  document.body.classList.add("dark-theme");
+} else {
+  localStorage.setItem("theme", "light");
+}
